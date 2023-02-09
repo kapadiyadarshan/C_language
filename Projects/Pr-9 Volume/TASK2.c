3679d2b3e74974d33d5713c963c14efc0ab3abe7
kapadiyadarshan <121553877+kapadiyadarshan@users.noreply.github.com>
2023-02-09 16:43:41 +0530

    Add files via upload

---
+++ Projects/Pr-9 Volume/TASK2.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+
+// students percentage
+
+struct Student
+{
+    int rollno;
+    char name[50];
+    float chem;
+    float phy;
+    float math;
+}stu[5];
+
+int main()
+{
+    int i;
+    float per,mark;
+
+    for(i=0; i<5; i++)
+    {
+        printf("\n---------------------------------------------------------------------\n\n");
+        printf("Roll No. = ");
+        scanf("%d",&stu[i].rollno);
+        fflush(stdin);
+
+        printf("Name = ");
+        scanf("%s",&stu[i].name);
+        fflush(stdin);
+
+        printf("Chemistry marks = ");
+        scanf("%f",&stu[i].chem);
+        fflush(stdin);
+
+        printf("Physics marks = ");
+        scanf("%f",&stu[i].phy);
+        fflush(stdin);
+
+        printf("Math's marks = ");
+        scanf("%f",&stu[i].math);
+        fflush(stdin);
+    }
+
+    printf("\n====================================================\n\n");
+
+    for(i=0; i<5; i++)
+    {
+        per = 0;
+
+        per = (stu[i].chem + stu[i].phy + stu[i].math) / 3 ;
+
+        printf("Roll no.   = %d\n",stu[i].rollno);
+        printf("Name       = %s\n",stu[i].name);
+        printf("Percentage = %f\n",per);
+
+        printf("\n---------------------------------------------------------------------\n\n");
+    }
+    return 0;
+}
